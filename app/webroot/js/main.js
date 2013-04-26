441439fa8c4a3fba8b66b08dba1fe1a33c096db6
Paula Faria <paulah37@gmail.com>
2013-04-26 03:05:37 -0300

    correcao da galeria de imagens

--- app/webroot/js/main.js
+++ app/webroot/js/main.js
@@ -4,7 +4,7 @@ var tpj=jQuery;
 
 tpj(document).ready(function() {
 
-	/* banner principal */
+  /* banner principal */
     if (tpj.fn.cssOriginal!=undefined)
        tpj.fn.css = tpj.fn.cssOriginal;
 
@@ -56,71 +56,119 @@ tpj(document).ready(function() {
                                             
             });
 
-	var jQuerycontainer = jQuery('#works');
+tpj('#gallery').revolution(
+            {    
+                delay:9000,                                                
+                startheight:390,                            
+                startwidth:840,
+                
+                hideThumbs:0,
+                
+                thumbWidth:161,                            
+                thumbHeight:137,
+                thumbAmount:4,
+                
+               navigationType:"thumb",               
+               navigationArrows:"nexttobullets",      
+               // navigationStyle:"round",               
+                                            
+               navigationHAlign:"center",             
+               navigationVAlign:"bottom",                 
+               navigationHOffset:0,
+               navigationVOffset:20,
+                
+               soloArrowLeftHalign:"left",
+               soloArrowLeftValign:"center",
+               soloArrowLeftHOffset:20,
+               soloArrowLeftVOffset:0,
+                
+               soloArrowRightHalign:"right",
+               soloArrowRightValign:"center",
+               soloArrowRightHOffset:20,
+               soloArrowRightVOffset:0,
+                touchenabled:"on",                      
+                onHoverStop:"on",                        
+                
+                navOffsetHorizontal:0,
+                navOffsetVertical:20,
+                
+                hideCaptionAtLimit:0,
+                hideAllCaptionAtLilmit:0,
+                hideSliderAtLimit:0,
+                
+                stopAtSlide:-1,
+                stopAfterLoops:-1,
+                
+                shadow:0,
+                fullWidth:"off"    
+                                            
+            });
+
+  var jQuerycontainer = jQuery('#works');
 
     window.container = jQuery('#works');
 
-	jQuerycontainer.isotope({
-		animationOptions: {
-	     duration: 750,
-	     easing: 'linear',
-	     queue: false
-	   }
-	});
+  jQuerycontainer.isotope({
+    animationOptions: {
+       duration: 750,
+       easing: 'linear',
+       queue: false
+     }
+  });
 
-	jQuery(".filter-link").click(function(){
+  jQuery(".filter-link").click(function(){
 
-		jQuery(this).parent().parent().find('a').removeClass('active');
+    jQuery(this).parent().parent().find('a').removeClass('active');
 
-		jQuery(this).addClass('active');
+    jQuery(this).addClass('active');
 
-		jQuery(".filter").slideUp(400);
+    jQuery(".filter").slideUp(400);
 
-		var selector = jQuery(this).attr('data-filter');
+    var selector = jQuery(this).attr('data-filter');
 
-		var etapa = jQuery('#filter-obras .active').attr('data-filter');
+    var etapa = jQuery('#filter-obras .active').attr('data-filter');
 
-		var etapaActive = jQuery('#filter-obras .active').html();
+    var etapaActive = jQuery('#filter-obras .active').html();
 
-		var bairro = jQuery('#filter-bairro .active').attr('data-filter');
+    var bairro = jQuery('#filter-bairro .active').attr('data-filter');
 
-		var bairroActive = jQuery('#filter-bairro .active').html();	
-		
-		jQuery('.etapa-active').html(etapaActive);
+    var bairroActive = jQuery('#filter-bairro .active').html(); 
+    
+    jQuery('.etapa-active').html(etapaActive);
 
-		jQuery('.bairro-active').html(bairroActive);		
+    jQuery('.bairro-active').html(bairroActive);    
 
-		if(etapa != '*' && bairro != '*'){	
+    if(etapa != '*' && bairro != '*'){  
 
-  			window.container.isotope({ filter: '.'+etapa+'.'+bairro });
+        window.container.isotope({ filter: '.'+etapa+'.'+bairro });
 
-  		}else if(etapa != '*'){
+      }else if(etapa != '*'){
 
-  			window.container.isotope({ filter: '.'+etapa });
+        window.container.isotope({ filter: '.'+etapa });
 
-  		}else if(bairro != '*'){
+      }else if(bairro != '*'){
 
-  			window.container.isotope({ filter: '.'+bairro });
+        window.container.isotope({ filter: '.'+bairro });
 
-  		}else{
+      }else{
 
         window.container.isotope({ filter: '*'});
 
       }
 
-  		return false
+      return false
       
-	});
+  });
 
-	jQuery(".filter-type-link").click(function(){
-		var typeFilter = jQuery(this).attr("type-filter");
-		jQuery("#filter-"+typeFilter).slideToggle(400);
-		return false;
-	});
+  jQuery(".filter-type-link").click(function(){
+    var typeFilter = jQuery(this).attr("type-filter");
+    jQuery("#filter-"+typeFilter).slideToggle(400);
+    return false;
+  });
 
-	/* mapa */
+  /* mapa */
 
-  	if(jQuery('body').find('#map-canvas').html()){initialize();}
+    if(jQuery('body').find('#map-canvas').html()){initialize();}
 
 
     jQuery('.link-investimento').click(function(){
